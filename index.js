d8f6cf651d8cd9d8e9c15d36a05ad197b03a1cba
ArShown <hles51723@gmail.com>
2022-05-05 11:40:05 +0800

    fix: 改用非同步模式解決執行被中斷問題

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const OpenCC = require("opencc");
 const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
+const ora = require("ora");
+
+const spinner = ora();
 
 const covertTypes = [
   "s2t",
@@ -21,78 +24,94 @@ const covertTypes = [
 ];
 
 program
-  .requiredOption(
+  .option(
     "-c, --covert <type>",
     "What type of covert: [s2t|t2s|s2tw|tw2s|s2hk|hk2s|s2twp|tw2sp|t2tw|hk2t|tw2t]",
     "tw2s"
   )
-  .requiredOption("-s, --source <path>", "source folder or file")
-  .requiredOption("-d, --destination <path>", "destination folder");
+  .option("-s, --source [path]", "source folder or file")
+  .option("-d, --destination [path]", "destination folder");
 
-program.parse(process.argv);
-const options = program.opts();
-
-let hasError = false;
-
-// check covert type
-if (!covertTypes.includes(options.covert)) {
-  console.log(`Not support this covert type: ${options.covert}!`);
-  console.log("Please check help.");
-  hasError = true;
+const configFromFile = () => {
+  const configPath = path.resolve(__dirname, './opencc.config.js');
+  if (fs.existsSync(configPath)) {
+    return require(configPath);
+  }
+  return {};
 }
 
-if (hasError) {
+try {
+  program.parse(process.argv);
+  const options = {...configFromFile(), ...program.opts()};
+
+  // check covert type
+  if (!covertTypes.includes(options.covert)) {
+    throw new Error(`Not support this covert type: ${options.covert}! Please check help.`);
+  }
+
+  // check source
+  if (!options.source) {
+    throw new Error("Need source folder or file");
+  }
+
+  // check destination
+  if (!options.destination) {
+    throw new Error("destination folder");
+  } else if(path.extname(options.destination) !== ""){
+    throw new Error("destination must be a folder");
+  }
+
+  const converter = new OpenCC(`${options.covert}.json`);
+
+  (async () => {
+    spinner.start();
+
+    const files = glob.sync(options.source);
+    for(let file of files){
+      if (fs.statSync(file).isDirectory()) {
+        const files2 = glob.sync(`${options.source}/**`, { nodir: true });
+        for(let file2 of files2){
+          await covertAndWriteFile(file2);
+        }
+      } else {
+        await covertAndWriteFile(file);
+      }
+    }
+
+    spinner.succeed("Success!");
+  })();
+
+  async function convert(text) {
+    var result = await converter.convertPromise(text);
+    const terms = options.terms ?? [];
+    for (let wordMap of terms) {
+      const matchTerm = new RegExp(wordMap[0], "g");
+      const isFind = result.match(matchTerm);
+      if (!isFind) {
+        continue;
+      }
+      result = result.replace(matchTerm, wordMap[1]);
+    }
+    return result;
+  }
+
+  async function covertAndWriteFile(file)  {
+    const data = fs.readFileSync(file, "utf-8");
+    const converted = await convert(data);
+    const diffPath = path.relative(path.dirname(options.source), file).replace(/\.\.\//, "");
+    const diffDirPath = path.join(
+      options.destination,
+      path.dirname(diffPath)
+    );
+    if (!fs.existsSync(diffDirPath)) {
+      fs.mkdirSync(diffDirPath, { recursive: true });
+    }
+    const destFile = path.join(options.destination, diffPath);
+    fs.writeFileSync(destFile, converted);
+    spinner.text = `${file} -> ${destFile}`;
+  }
+
+} catch (err) {
+  spinner.fail(err.message);
   process.exit();
 }
-
-const converter = new OpenCC(`${options.covert}.json`);
-
-glob(options.source, {}, function (er, files) {
-  files.forEach(function (file) {
-    if (fs.statSync(file).isDirectory()) {
-      glob(`${options.source}/**`, { nodir: true }, (er2, files2) => {
-        files2.forEach(function (file2) {
-          covertAndWriteFile(file2);
-        });
-      });
-    } else {
-      covertAndWriteFile(file);
-    }
-  });
-});
-
-function covertAndWriteFile(file) {
-  fs.readFile(file, "utf8", function (err, data) {
-    const fileName = path.basename(file);
-
-    converter.convertPromise(data).then((converted) => {
-      const diffPath = path
-        .relative(path.dirname(options.source), file)
-        .replace(/\.\.\//, "");
-      const diffDirPath = path.join(
-        options.destination,
-        path.dirname(diffPath)
-      );
-
-      if (!fs.existsSync(diffDirPath)) {
-        fs.mkdirSync(diffDirPath, { recursive: true }, (err) => {
-          if (err) {
-            console.error("mkdir err: ", err);
-            process.exit();
-          }
-        });
-      }
-
-      fs.writeFile(
-        path.join(options.destination, diffPath),
-        converted,
-        (err) => {
-          if (err) console.error("writeFile err: ", err);
-          process.exit();
-        }
-      );
-    });
-  });
-}
-
-console.info("\x1b[32m", "success!");
